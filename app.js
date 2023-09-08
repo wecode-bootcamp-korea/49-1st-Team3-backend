31172dee68bd2065ed9b884b9951bd97c8fb4500
Seungwon Kim <kim.sw94@gmail.com>
2023-09-08 19:23:11 +0900

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = require('express');
+const http = require('http');
+const cors = require('cors');
+const typorm = require('typeorm');
+
+
+
+
+
+
+const 
+
+
+
+
+server.listen(8000, ("server is listening on port 8000"))
+
+
+console.log("test");
+
+
+app.post("/signup", async(req, res)=> {
+    try {
+
+    } catch {
+
+    }
+}
+)
