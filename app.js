f53dde2907f95995c75d142cf475c2cbf7634850
Seungwon Kim <kim.sw94@gmail.com>
2023-09-11 14:16:26 +0900

    backend 초기 세팅

--- app.js
+++ app.js
@@ -2,28 +2,13 @@ const app = require('express');
 const http = require('http');
 const cors = require('cors');
 const typorm = require('typeorm');
+const app = require('express');
+const jwt = require('jsonwebtoken');
 
+app = express();
 
-
-
-
-
-const 
-
-
-
-
-server.listen(8000, ("server is listening on port 8000"))
-
-
-console.log("test");
-
-
-app.post("/signup", async(req, res)=> {
-    try {
-
-    } catch {
-
-    }
-}
-)
+const start = async() => {
+    server.listen(8000, => console.log("server is listening on port 8000"))
+} //서버를 시작합니다.
+ 
+start ();
