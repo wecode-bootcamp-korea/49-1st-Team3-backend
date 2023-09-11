55208b93bf76c83dd06610e3b6b403fbd869701d
Seungwon Kim <kim.sw94@gmail.com>
2023-09-11 20:39:02 +0900

    updated

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const app = require('express');
-const http = require('http');
-const cors = require('cors');
-const typorm = require('typeorm');
+const http = require('http'); // http 프로토콜 사용
+const cors = require('cors'); //cors에 대해 알아야함
+const typorm = require('typeorm'); //typeorm에 대해 알아야함
 const app = require('express'); // express 다운로드
 const jwt = require('jsonwebtoken'); // 로그인을 위한 확인
 const morgan = require('morgan'); // morgan이 뭔지 알아야함
 
 app = express();
+
+app.use(express.json());
+app.use(morgan.dev());
+
+appDataSource.initialize().then(() => {
+    console.log("Datasource inialized!");
+  });
 
 app.post("/usercreation", ("./library/post"))
 app.get("/null")
