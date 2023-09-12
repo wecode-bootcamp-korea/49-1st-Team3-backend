915f0ecfa6ec3dce5a415f618304960dbf8ab752
lee ho young <ihoyeong215@gmail.com>
2023-09-13 05:41:22 +0900

    version_1

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const http = require('http')
 const express = require('express')
 const cors = require('cors')
-const { DataSource, CancellationToken } = require('typeorm');
-const { createDecipheriv } = require('crypto');
-const { error } = require('console');
-const app = express();
+const { DataSource }  = require("typeorm")
+// database 정보를 이용해서 새로운 연결을 만든다
+const userService = require("./services/userService")
 
+const myDataSource = new DataSource ({
+  // 연결을 할 수 있는 데이터 베이스를 준다
+  type:'mysql',
+  host: 'localhost',
+  port: 3306,
+  username:'root',
+  password:'lhy',
+  database: 'wethread'
+})
+
+const app = express();
 app.use(cors());
 
-const myDataSource = new DataSource({
-  type: 'mysql', 
-  host: '127.0.0.1', 
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'westagram'
- })
+
 
 
 app.use(express.json()) // for parsing application/json
 
 app.get("/", async(req,res) => {
   try{
-    return res.status(200).json("Welcome")
+    return res.status(200).json("welcome!")
   } catch(error) {
     console.log(error);
   }
@@ -39,11 +42,13 @@ const start = async () => { // 서버를 시작하는 함수입니다.
 }
 
 start();
+ 
+myDataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!")
+})
 
-myDataSource.initialize().then(
-    () => console.log("datasource initialized")
-)
+app.post("/logIn", userService.logIn)
 
-
-app.get("/posts")
-app.post("/users")
+// app.get("/posts")
+// app.post("/signUp", userServise.signUp)  sooah
+// app.post("/logIn", logIn)
---
+++ services/userService.js
@@ -0,0 +1,62 @@
+const { json } = require('body-parser');
+const { log } = require('console');
+const { appendFile } = require('fs');
+const { request } = require('http');
+const { DataSource } = require('typeorm');
+
+
+const myDataSource = new DataSource({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'lhy',
+    database: 'wethread'
+})
+
+// sooah - signup
+// hoyoung - login
+// sw - createThread
+
+
+
+const logIn = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log(email, password)
+        // 만약에 email password가 공란이면 
+        if (email === undefined || password === undefined) {
+            // error를 만들어서
+            const error = new Error("공란입니다~")
+            // error는 오류 코드 400이다
+            error.statusCode = 400
+            // error를 던져라
+            throw error
+        }
+
+        const userCheckout = await myDataSource.query(`
+        SELECT email, password FROM users WHERE email = "${email}" 
+        `);
+
+        if (!userCheckout == email) {
+            const error = new Error('잘못된 이메일 입니다.')
+            error.statusCode = 400
+            throw error
+        }
+
+        return res.status(201).json({
+            "message": "로그인 성공!"
+        });
+
+    } catch (error) {
+        console.error('로그인 오류 발생:', error);
+        return res.status(500), json({
+            error: '계정 또는 비밀번호를 확인해주세요.',
+        });
+    }
+}
+
+// 다른 파일에서도 쓸 수 있게 만들어준다
+module.exports = {
+    logIn
+}
