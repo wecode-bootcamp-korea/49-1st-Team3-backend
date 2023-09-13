f4dcd4c064f4780f73b9f6e73de1927631c47a33
Seungwon Kim <kim.sw94@gmail.com>
2023-09-13 13:52:08 +0900

    update

--- threadServices/threads.js
+++ threadServices/threads.js
@@ -15,12 +15,12 @@ const postDeletion = async(req, res) => {
     return res.status(201).json("POST_DELETED");
 }
 
-const postRead = async(req, res) => {
-    const postReadId = req.body;
-    postReadId = postReadId.id;
-    const readPost = await myDataSource.query(`SELECT * FROM posts WHERE id = '${postReadId}'`)
-    return res.status(201).json("POST_READ");
-}
+// const postRead = async(req, res) => {
+//     const postReadId = req.body;
+//     postReadId = postReadId.id;
+//     const readPost = await myDataSource.query(`SELECT * FROM posts WHERE id = '${postReadId}'`)
+//     return res.status(201).json("POST_READ");
+// }
 
 const postUpdate = async(req, res) => {
     const postUpdateData = req.body;
@@ -31,10 +31,20 @@ const postUpdate = async(req, res) => {
 
 module.exports = {
     postCreation,
-    postUpdate,
-    postDeletion,
+    // postUpdate,
+    // postDeletion,
     postRead
 }
 
 
+// 회의록 작성
+// ror 설정 위함
+// 스프린트 별로 context 파악
+// 빙향성도 추가해서
 
+
+참
+
+
+// const = likesNumber = Object.values(id)
+// array.length(likesNumber) + 1
