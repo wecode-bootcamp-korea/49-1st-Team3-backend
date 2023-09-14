69ef5ea93a7235d7774599c1e1ff92e205055379
SooahKim <22tndk@gamil.com>
2023-09-14 22:14:27 +0900

    signupcomplete

--- db/migrations/20230912080444_create_users_table.sql
+++ db/migrations/20230912080444_create_users_table.sql
@@ -5,8 +5,10 @@ CREATE TABLE users {
     email VARCHAR(50) NOT NULL,
     password VARCHAR(200) NOT NULL,
     nickname VARCHAR(200) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-    PRIMARY KEY (id)
+    profile_image VARCHAR(1000) NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id),
+    user_id INT,
     FOREIGN KEY (user_id) REFERENCES users (id)
 }
 
@@ -15,8 +17,8 @@ CREATE TABLE threads {
     user_id INT NOT NULL,
     content VARCHAR(2000) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
-    PRIMARY KEY (id)
+    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 }
 
@@ -24,9 +26,11 @@ CREATE TABLE likes {
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     thread_id INT NOT NULL,
-    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
-    PRIMARY KEY (id)
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (thread_id) REFERENCES threads (id)
+
 }
 
 CREATE TABLE comments {
